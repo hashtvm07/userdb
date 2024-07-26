6cb45ea670eff605072730b33526baf97cc2ce3b
Muhammed Hashim <muhammed.hashim@maqta.ae>
2024-07-26 11:30:48 +0400

    initial files

---
+++ Source/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/database");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
