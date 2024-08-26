f0bda30444b794b7eac9e702a8e2d38699e8b795
Muhammed Hashim <muhammed.hashim@maqta.ae>
2024-08-26 06:24:45 +0400

    web.config

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import app from './src/app';
 
 console.log('reached at server.ts')
 // const PORT: number = parseInt(process.env.PORT || '4000', 10);
-const PORT = 80; //process.env.PORT || 4000;
+const PORT = 4010; //process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
